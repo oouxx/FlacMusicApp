65f7ac7fb81758a474957056f7a3aea51cad1497
oouxx <wangxinxing961129@gmail.com>
2026-03-28 07:55:23 +0800

    Initial Commit

---
+++ FlacMusicApp/FlacMusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlacMusicAppApp.swift
+//  FlacMusicApp
+//
+//  Created by wxx on 2026/3/28.
+//
+
+import SwiftUI
+
+@main
+struct FlacMusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
